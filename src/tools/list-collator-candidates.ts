d2d88f296b0cb97e7392a1c4ee754fa4dab10007
Crystalin <alan@purestake.com>
2022-01-24 12:33:44 -0500

    Clean names

--- src/tools/list-collator-candidates.ts
+++ src/tools/list-collator-candidates.ts
@@ -41,7 +41,10 @@ const main = async () => {
     const candidate = v[1].unwrap();
     p[candidate.id.toString()] = {
       id: candidate.id.toString(),
-      name: candidateNames[index],
+      name: candidateNames[index].replace(
+        /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
+        ""
+      ),
       totalDelegators: 0,
       isActive: candidate.state.toString() == "Active",
       totalDelegations: candidate.totalCounted.toBigInt(),
