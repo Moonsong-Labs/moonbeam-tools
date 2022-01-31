3ddfe1b0d83051d20573a713c00158cec5c72349
Crystalin <alan@purestake.com>
2022-01-31 17:32:35 -0500

    Improves tx stream

--- src/utils/transactions.ts
+++ src/utils/transactions.ts
@@ -30,25 +30,37 @@ export const sendAllAndWaitLast = async (extrinsics: SubmittableExtrinsic[]) => 
 export const sendAllStreamAndWaitLast = async (
   api: ApiPromise,
   extrinsics: SubmittableExtrinsic[],
-  { threshold = 500, batch = 200 } = {threshold: 500, batch : 200}
+  { threshold = 500, batch = 200, timeout = 120000 } = {
+    threshold: 500,
+    batch: 200,
+    timeout: 120000,
+  }
 ) => {
   let promises = [];
+  let lastUpdateTime = Date.now();
   while (extrinsics.length > 0) {
     const pending = await api.rpc.author.pendingExtrinsics();
     if (pending.length < threshold) {
       const chunk = extrinsics.splice(0, Math.min(threshold - pending.length, batch));
-      console.log(`Sending ${chunk.length}tx (${extrinsics.length} left)`)
+      console.log(`Sending ${chunk.length}tx (${extrinsics.length} left)`);
       promises.push(
         Promise.all(
           chunk.map((tx) => {
             return new Promise(async (resolve, reject) => {
+              const timer = setTimeout(() => {
+                reject(`timed out`);
+                unsub();
+              }, timeout);
               const unsub = await tx.send((result) => {
+                // reset the timer
                 if (result.isError) {
                   console.log(result.toHuman());
+                  clearTimeout(timer);
                   reject(result.toHuman());
                 }
                 if (result.isInBlock) {
                   unsub();
+                  clearTimeout(timer);
                   resolve(null);
                 }
               });
