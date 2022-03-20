378e708d5867c09bc4f4b6a40d62f27f13183337
4meta5 <asinghchrony@protonmail.com>
2022-03-20 16:11:11 -0400

    static typing resolves BigInt math errors

--- src/tools/check-unreserved-balance.ts
+++ src/tools/check-unreserved-balance.ts
@@ -48,86 +48,91 @@ const main = async () => {
     apiAt.query.parachainStaking.delegatorState.entries(),
   ]);
   // ACTUAL AMOUNT RESERVED FOR ALL ACCOUNTS
-  const reservedAccounts = accountBalances.reduce((p, v) => {
-    const accountData = v[1];
-    const accountId = `0x${v[0].toHex().slice(-40)}`;
-    const reserved = accountData.data.reserved.toBigInt();
-    if (!p[accountId]) {
-      p[accountId] = {
-        accountId,
-        reserved: 0n,
-      };
-    }
-    p[accountId].reserved += reserved;
-    return p;
-  }, {});
-  console.log(reservedAccounts);
+  const reservedAccounts: { [accountId: string]: { accountId: string; reserved: bigint } } =
+    accountBalances.reduce((p, v) => {
+      const accountId = `0x${v[0].toHex().slice(-40)}`;
+      const reserved = v[1].data.reserved.toBigInt();
+      if (!p[accountId]) {
+        p[accountId] = {
+          accountId,
+          reserved,
+        };
+      }
+      return p;
+    }, {});
   // EXPECTED RESERVED = STAKING (CANDIDATE || DELEGATOR) + AUTHOR MAPPING +
   // PROXY + TREASURY
-  const treasuryDeposits = treasuryProposals.reduce((p, v) => {
-    const treasuryProposal = v[1].unwrap();
-    const accountId = `0x${treasuryProposal.proposer.toHex().slice(-40)}`;
-    const reserved = treasuryProposal.bond.toBigInt();
-    if (!p[accountId]) {
-      p[accountId] = {
-        accountId,
-        reserved: 0n,
-      };
-    }
-    p[accountId].reserved += reserved;
-    return p;
-  }, {});
-  const proxyDeposits = proxies.reduce((p, v) => {
-    const reserved = v[1][1].toBigInt();
-    const accountId = `0x${v[0].toHex().slice(-40)}`;
-    if (!p[accountId]) {
-      p[accountId] = {
-        accountId,
-        reserved: 0n,
-      };
-    }
-    p[accountId].reserved += reserved;
-    return p;
-  }, {});
-  const authorMappingDeposits = mappingWithDeposit.reduce((p, v) => {
-    const registrationInfo = v[1].unwrap();
-    const accountId = `0x${registrationInfo.account.toHex().slice(-40)}`;
-    const reserved = registrationInfo.deposit;
-    if (!p[accountId]) {
-      p[accountId] = {
-        accountId,
-        reserved: 0n,
-      };
-    }
-    p[accountId].reserved += reserved;
-    return p;
-  }, {});
-  const candidateDeposits = candidateInfo.reduce((p, v) => {
-    const candidate = v[1].unwrap();
-    const id = `0x${v[0].toHex().slice(-40)}`;
-    const reserved = candidate.bond;
-    if (!p[id]) {
-      p[id] = {
-        id,
-        reserved: 0n,
-      };
-    }
-    p[id].reserved += reserved;
-    return p;
-  }, {});
-  const delegatorDeposits = delegatorState.reduce((p, v) => {
-    const delegator = v[1].unwrap();
-    const id = `0x${v[0].toHex().slice(-40)}`;
-    const reserved = delegator.total;
-    if (!p[id]) {
-      p[id] = {
-        id,
-        reserved: 0n,
-      };
-    }
-    p[id].reserved += reserved;
-    return p;
-  }, {});
+  const treasuryDeposits: { [accountId: string]: { accountId: string; reserved: bigint } } =
+    treasuryProposals.reduce((p, v) => {
+      const treasuryProposal = v[1].unwrap();
+      const accountId = `0x${treasuryProposal.proposer.toHex().slice(-40)}`;
+      const reserved = treasuryProposal.bond.toBigInt();
+      if (!p[accountId]) {
+        p[accountId] = {
+          accountId,
+          reserved: 0n,
+        };
+      }
+      p[accountId].reserved += reserved;
+      return p;
+    }, {});
+  const proxyDeposits: { [accountId: string]: { accountId: string; reserved: bigint } } =
+    proxies.reduce((p, v) => {
+      const reserved = v[1][1].toBigInt();
+      const accountId = `0x${v[0].toHex().slice(-40)}`;
+      if (!p[accountId]) {
+        p[accountId] = {
+          accountId,
+          reserved: 0n,
+        };
+      }
+      p[accountId].reserved += reserved;
+      return p;
+    }, {});
+  const authorMappingDeposits: { [accountId: string]: { accountId: string; reserved: bigint } } =
+    mappingWithDeposit.reduce((p, v) => {
+      const registrationInfo = v[1].unwrap();
+      const accountId = `0x${registrationInfo.account.toHex().slice(-40)}`;
+      const reserved = registrationInfo.deposit;
+      if (!p[accountId]) {
+        p[accountId] = {
+          accountId,
+          reserved: 0n,
+        };
+      }
+      p[accountId].reserved += reserved;
+      return p;
+    }, {});
+  const candidateDeposits: { [accountId: string]: { accountId: string; reserved: bigint } } =
+    candidateInfo.reduce((p, v) => {
+      const candidate = v[1].unwrap();
+      const id = `0x${v[0].toHex().slice(-40)}`;
+      const reserved = candidate.bond;
+      if (!p[id]) {
+        p[id] = {
+          id,
+          reserved: 0n,
+        };
+      }
+      p[id].reserved += reserved;
+      return p;
+    }, {});
+  const delegatorDeposits: { [accountId: string]: { accountId: string; reserved: bigint } } =
+    delegatorState.reduce((p, v) => {
+      const delegator = v[1].unwrap();
+      const id = `0x${v[0].toHex().slice(-40)}`;
+      const reserved = delegator.total;
+      if (!p[id]) {
+        p[id] = {
+          id,
+          reserved: 0n,
+        };
+      }
+      p[id].reserved += reserved;
+      return p;
+    }, {});
+  var negativeImbalanceRequiresHotfixExtrinsic = false;
+  const imbalancesToFix = [];
   const allDeposits: { [accountId: string]: bigint } = [
     ...Object.keys(authorMappingDeposits),
     ...Object.keys(candidateDeposits),
@@ -139,25 +144,48 @@ const main = async () => {
     if (p[accountId]) {
       return p;
     }
+    // const deposits = [{}]; and sum them to print if hotfix required
     const expectedReserved: bigint =
-      (authorMappingDeposits[accountId] ? authorMappingDeposits[accountId].reserved : 0n) +
-      (candidateDeposits[accountId] ? candidateDeposits[accountId].reserved : 0n) +
-      (delegatorDeposits[accountId] ? delegatorDeposits[accountId].reserved : 0n) +
-      (treasuryDeposits[accountId] ? treasuryDeposits[accountId].reserved : 0n) +
-      (proxyDeposits[accountId] ? proxyDeposits[accountId].reserved : 0n);
+      authorMappingDeposits[accountId].reserved +
+      candidateDeposits[accountId].reserved +
+      delegatorDeposits[accountId].reserved +
+      treasuryDeposits[accountId].reserved +
+      proxyDeposits[accountId].reserved;
     if (expectedReserved != reservedAccounts[accountId].reserved) {
       console.log("Printing different RESERVED and EXPECTED_RESERVED for ", accountId);
+      if (reservedAccounts[accountId].reserved < expectedReserved) {
+        negativeImbalanceRequiresHotfixExtrinsic = true;
+        console.log(
+          "BUG REQUIRES HOTFIX EXTRINSIC TO CORRECT ACCOUNT: ",
+          accountId,
+          "RESERVED: ",
+          reservedAccounts[accountId].reserved,
+          "EXPECTED RESERVED: ",
+          expectedReserved
+        );
+      }
+      const dueToBeUnreserved = reservedAccounts[accountId].reserved - expectedReserved;
       console.log(
         "RESERVED: ",
         reservedAccounts[accountId].reserved,
         "EXPECTED RESERVED: ",
-        expectedReserved
+        expectedReserved,
+        "POSITIVE DIFFERENCE: ",
+        dueToBeUnreserved
       );
+      // COLLECT INTO OUTPUT
+      imbalancesToFix.push({ accountId, dueToBeUnreserved });
     }
     p[accountId] = expectedReserved;
     return p;
   }, {});
-
+  console.log("DUE TO BE UNRESERVED: \n", imbalancesToFix);
+  if (negativeImbalanceRequiresHotfixExtrinsic) {
+    console.log("FIX REQUIRES HOTFIX EXTRINSIC TO FIX NEGATIVE RESERVE IMBALANCE(S)");
+    // look for console output prefixed by
+    // `BUG REQUIRES HOTFIX EXTRINSIC TO CORRECT ACCOUNT: `
+  }
+  // TODO: propose and send as democracy proposal
   api.disconnect();
 };
 
