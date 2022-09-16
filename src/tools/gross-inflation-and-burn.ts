12052707e25ef486cb3448d5bd4a76a805d32758
gorka <gorka.irazoki@gmail.com>
2022-09-16 16:13:24 +0200

    Introduce script to calculate gross inflation and burn

---
+++ src/tools/gross-inflation-and-burn.ts
@@ -0,0 +1,95 @@
+// This script is expected to run against a parachain network (using launch.ts script)
+import { ApiPromise, WsProvider } from "@polkadot/api";
+import yargs from "yargs";
+import { promiseConcurrent, getApiFor } from "..";
+import { BN } from "@polkadot/util";
+
+const TREASURY = "0x6d6f646C70792f74727372790000000000000000";
+const argv = yargs(process.argv.slice(2))
+  .usage("Usage: $0")
+  .version("1.0.0")
+  .options({
+    url: {
+      type: "string",
+      description: "Websocket url",
+      string: true,
+      demandOption: true,
+    },
+    from: {
+      type: "number",
+      description: "from block number (included)",
+      demandOption: true,
+    },
+    to: {
+      type: "number",
+      description: "to block number (included)",
+      demandOption: true,
+    },
+  }).argv;
+
+const printDOTs = (value: bigint, decimals = 4) => {
+  const power = 10n ** (10n - BigInt(decimals));
+  const decimal_power = 10 ** decimals;
+  if (decimals > 0) {
+    return (Number(value / power) / decimal_power).toFixed(decimals).padStart(5 + decimals, " ");
+  }
+  return (value / power).toString().padStart(5, " ");
+};
+
+const main = async () => {
+  const api = await getApiFor(argv);
+
+  const toBlockNumber = argv.to;
+  const fromBlockNumber = argv.from;
+  // Get from block hash and totalSupply
+  const fromPreBlockHash = (await api.rpc.chain.getBlockHash(fromBlockNumber - 1)).toString();
+  const fromPreSupply = await (await api.at(fromPreBlockHash)).query.balances.totalIssuance();
+  const fromTreasuryPre = (await (await api.at(fromPreBlockHash)).query.system.account(TREASURY)).data.free;
+
+  let theoreticalSupplyIncrease = new BN(fromPreSupply);
+  let onlyRewards = new BN(0);
+
+  // Get to block hash and totalSupply
+  const toBlockHash = (await api.rpc.chain.getBlockHash(toBlockNumber)).toString();
+  const toSupply = await (await api.at(toBlockHash)).query.balances.totalIssuance();
+  const toTreasuryPre = (await (await api.at(toBlockHash)).query.system.account(TREASURY)).data.free;
+
+  console.log(`========= Checking block ${fromBlockNumber}...${toBlockNumber}`);
+  let blockNumbers = [];
+  for (let i = argv.from; i <= argv.to; i++) {
+    blockNumbers.push(i);
+  }
+
+  await promiseConcurrent(
+    80,
+    async (blockNumber) => {
+      const blockHash = await api.rpc.chain.getBlockHash(blockNumber);
+      const apiAt = await api.at(blockHash);
+      const records = (await apiAt.query.system.events()) as any;
+
+      const events = records.filter(
+        ({ event }) => event.section == "parachainStaking" && (event.method == "Rewarded" || event.method == "ReservedForParachainBond")
+      );
+      for (const event of events) {
+        const [account, amount] = event.event.data as any;
+        theoreticalSupplyIncrease = theoreticalSupplyIncrease.add(new BN(amount));
+        onlyRewards = onlyRewards.add(new BN(amount))
+      }
+    },
+    blockNumbers
+  );
+
+  const burntFees = new BN(theoreticalSupplyIncrease).sub(toSupply);
+
+  // Log difference in supply, we should be equal to the burnt fees
+  console.log(
+    `  supply diff: ${(fromPreSupply.toBigInt() - toSupply.toBigInt())
+      .toString()
+      .padStart(30, " ")}`
+  );
+  console.log(`  burnt fees : ${burntFees.toString().padStart(30, " ")}`);
+  console.log(`  gross inflation : ${theoreticalSupplyIncrease.toString().padStart(30, " ")}`);
+  await api.disconnect();  
+};
+
+main();
