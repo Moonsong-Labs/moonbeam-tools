4c282b524066d09dd1e76de1eed5b74d20353bf9
Crystalin <alan@purestake.com>
2022-10-24 18:22:53 -0400

    Fixes reserve to lock script

--- src/hotfixes/runtime-1900-fix-staking-reserve-to-lock-migration.ts
+++ src/hotfixes/runtime-1900-fix-staking-reserve-to-lock-migration.ts
@@ -15,13 +15,9 @@ Ex: ./node_modules/.bin/ts-node-transpile-only runtime-1900-fix-staking-reserve-
 import yargs from "yargs";
 import "@polkadot/api-augment";
 import "@moonbeam-network/api-augment";
-import { Keyring } from "@polkadot/api";
+import { ApiPromise, Keyring } from "@polkadot/api";
 import { getApiFor, NETWORK_YARGS_OPTIONS } from "../utils/networks";
-import { BN, u8aToHex } from "@polkadot/util";
-import { blake2AsHex, xxhashAsU8a } from "@polkadot/util-crypto";
-import { promiseConcurrent } from "../utils/functions";
-import { AnyTuple } from "@polkadot/types-codec/types";
-import { StorageKey } from "@polkadot/types";
+import { blake2AsHex, xxhashAsHex } from "@polkadot/util-crypto";
 
 const argv = yargs(process.argv.slice(2))
   .usage("Usage: $0")
@@ -59,60 +55,67 @@ const argv = yargs(process.argv.slice(2))
 
 async function main() {
   const api = await getApiFor(argv);
-
   const keyring = new Keyring({ type: "ethereum" });
-  const atBlock = argv["at-block"]
-    ? new BN(argv["at-block"])
-    : (await api.rpc.chain.getBlock()).block.header.number.toBn();
-  const blockHash = await api.rpc.chain.getBlockHash(atBlock);
-  const apiAt = await api.at(blockHash);
-
-  const collectiveThreshold = argv["collective-threshold"] || 1;
-  const proposalAmount = api.consts.democracy.minimumDeposit;
-
-  let account;
-  let nonce;
-  if (argv["account-priv-key"]) {
-    account = keyring.addFromUri(argv["account-priv-key"], null, "ethereum");
-    const { nonce: rawNonce, data: balance } = (await api.query.system.account(
-      account.address
-    )) as any;
-    nonce = BigInt(rawNonce.toString());
-  }
 
   try {
-    const palletEncoder = new TextEncoder().encode("ParachainStaking");
-    const palletHash = xxhashAsU8a(palletEncoder, 128);
-    const delegatorReserveToLockMigrations = new TextEncoder().encode(
-      "DelegatorReserveToLockMigrations"
-    );
-    const collatorReserveToLockMigrations = new TextEncoder().encode(
-      "CollatorReserveToLockMigrations"
-    );
-    const storageHashDelegator = xxhashAsU8a(delegatorReserveToLockMigrations, 128);
-    const storageHashCollator = xxhashAsU8a(collatorReserveToLockMigrations, 128);
+    const atBlock =
+      argv["at-block"] || (await api.rpc.chain.getBlock()).block.header.number.toNumber();
+    const blockHash = await api.rpc.chain.getBlockHash(atBlock);
 
-    const keyDelegator = new Uint8Array([...palletHash, ...storageHashDelegator]);
-    const keyCollator = new Uint8Array([...palletHash, ...storageHashCollator]);
+    const collectiveThreshold = argv["collective-threshold"] || 1;
+    const proposalAmount = api.consts.democracy.minimumDeposit;
+
+    let account;
+    let nonce;
+    if (argv["account-priv-key"]) {
+      account = keyring.addFromUri(argv["account-priv-key"], null, "ethereum");
+      const { nonce: rawNonce, data: balance } = (await api.query.system.account(
+        account.address
+      )) as any;
+      nonce = BigInt(rawNonce.toString());
+    }
+
+    async function getAllKeys(api: ApiPromise, prefix: string, startKey?: string) {
+      const keys = (
+        await api.rpc.state.getKeysPaged(prefix, 1000, startKey || prefix, blockHash)
+      ).map((d) => d.toHex());
+
+      if (keys.length == 0) {
+        return [];
+      }
+      return keys.concat(await getAllKeys(api, prefix, keys[keys.length - 1]));
+    }
+
+    const delegatorPrefix =
+      xxhashAsHex("ParachainStaking", 128) +
+      xxhashAsHex("DelegatorReserveToLockMigrations", 128).slice(2);
+    const collatorPrefix =
+      xxhashAsHex("ParachainStaking", 128) +
+      xxhashAsHex("CollatorReserveToLockMigrations", 128).slice(2);
+
+    const delegatorKeys = await getAllKeys(api, delegatorPrefix);
+    const collatorKeys = await getAllKeys(api, collatorPrefix);
+
     console.log(
-      `DelegatorReserveToLockMigrations ${u8aToHex(
-        keyDelegator
-      )}\nCollatorReserveToLockMigrations  ${u8aToHex(keyCollator)}`
+      `DelegatorReserveToLockMigrations: ${delegatorKeys.length
+        .toString()
+        .padStart(6, " ")} (prefix: ${delegatorPrefix})`
+    );
+    console.log(
+      ` CollatorReserveToLockMigrations: ${collatorKeys.length
+        .toString()
+        .padStart(6, " ")} (prefix: ${collatorPrefix})`
     );
 
-    const txKillStorage = api.tx.utility.batch(
-      [keyDelegator, keyCollator].map((k) =>
-        api.tx.system.killPrefix(api.query.parachainStaking.atStake.keyPrefix(k), 1)
-      )
-    );
-    const toPropose = api.tx.scheduler.scheduleAfter(0, null, 0, {
-      Value: txKillStorage,
-    });
+    const toPropose = api.tx.utility.batch([
+      api.tx.system.killPrefix(delegatorPrefix, delegatorKeys.length),
+      api.tx.system.killPrefix(collatorPrefix, collatorKeys.length),
+    ]);
     let encodedProposal = toPropose?.method.toHex() || "";
     let encodedHash = blake2AsHex(encodedProposal);
-    // console.log("Encoded proposal after schedule is", encodedProposal);
-    // console.log("Encoded proposal hash after schedule is", encodedHash);
-    // console.log("Encoded length", encodedProposal.length);
+    console.log("Encoded proposal after schedule is", encodedProposal);
+    console.log("Encoded proposal hash after schedule is", encodedHash);
+    console.log("Encoded length", encodedProposal.length);
 
     if (argv["sudo"]) {
       await api.tx.sudo.sudo(toPropose).signAndSend(account, { nonce: nonce++ });
