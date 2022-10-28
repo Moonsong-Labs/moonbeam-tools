a9bb3bd0cd2224f05096b75c157c938146fd825c
Crystalin <alan@purestake.com>
2022-10-28 00:01:04 -0400

    Adds script to run moonbeam fork (wip)

--- src/libs/helpers/state-manipulator/state-manager.ts
+++ src/libs/helpers/state-manipulator/state-manager.ts
@@ -1,6 +1,6 @@
 import Debug from "debug";
 import fs from "node:fs/promises";
-import { request, stream } from "undici";
+import { Client } from "undici";
 import path from "node:path";
 import { processState } from "./genesis-parser";
 import { RoundManipulator } from "./round-manipulator";
@@ -26,7 +26,14 @@ export const STORAGE_NAMES: { [name in NetworkName]: string } = {
 
 // Downloads the exported state from s3. Only if the xxx-chain-info.json file hasn't changed
 // 2 files are created:
-export async function downloadExportedState(network: NetworkName, outPath: string) {
+export async function downloadExportedState(
+  network: NetworkName,
+  outPath: string,
+  checkLatest = true,
+  onStart?: (size: number) => void,
+  onProgress?: (bytes: number) => void,
+  onComplete?: () => void
+) {
   if (!STORAGE_NAMES[network]) {
     throw new Error(
       `Invalid network ${network}, expecting ${Object.keys(STORAGE_NAMES).join(", ")}`
@@ -39,21 +46,41 @@ export async function downloadExportedState(network: NetworkName, outPath: strin
   const stateFile = path.join(outPath, stateFileName);
 
   debug(`Checking ${STORAGE_NAMES[network]} in ${stateInfoFile}`);
-  const downloadedStateInfo = await (
-    await request(
-      `https://s3.us-east-2.amazonaws.com/` +
-        `snapshots.moonbeam.network/${STORAGE_NAMES[network]}/` +
-        `latest/${STORAGE_NAMES[network]}-chain-info.json`
-    )
-  ).body.json();
 
   await fs.mkdir(outPath, { recursive: true });
 
+  const stateInfoExists = await fs
+    .access(stateInfoFile)
+    .then(() => true)
+    .catch(() => null);
+  const stateExist = await fs
+    .access(stateInfoFile)
+    .then(() => true)
+    .catch(() => false);
+
+  // No check for latest, skip if files already exists
+  if (stateInfoExists && stateExist && !checkLatest) {
+    return stateFile;
+  }
+  
   const stateInfo = await fs
     .readFile(stateInfoFile)
     .then((d) => JSON.parse(d.toString()))
     .catch(() => null);
+    
+  const client = new Client(`https://s3.us-east-2.amazonaws.com`);
+  const downloadedStateInfo = await (
+    await client.request({
+      path:
+        `/snapshots.moonbeam.network/${STORAGE_NAMES[network]}/` +
+        `latest/${STORAGE_NAMES[network]}-chain-info.json`,
+      method: "GET",
+    })
+  ).body.json();
+  
+  // Already latest version
   if (stateInfo && stateInfo.best_hash == downloadedStateInfo.best_hash) {
+    client.close();
     return stateFile;
   }
 
@@ -62,13 +89,46 @@ export async function downloadExportedState(network: NetworkName, outPath: strin
   debug(
     `Preparing to download ${stateFileName} (best-hash: ${downloadedStateInfo.best_hash}) to ${stateFile}`
   );
-  await stream(
-    `https://s3.us-east-2.amazonaws.com/` +
-      `snapshots.moonbeam.network/${STORAGE_NAMES[network]}/` +
-      `latest/${STORAGE_NAMES[network]}-state.json`,
-    { method: "GET" },
-    () => fileStream
-  );
+
+  let transferredBytes = 0;
+  await new Promise<void>((resolve, reject) => {
+    client.dispatch(
+      {
+        path:
+          `/snapshots.moonbeam.network/${STORAGE_NAMES[network]}/` +
+          `latest/${STORAGE_NAMES[network]}-state.json`,
+        method: "GET",
+      },
+      {
+        onConnect: () => {},
+        onError: (error) => {
+          reject(error);
+        },
+        onHeaders: (statusCode, headers) => {
+          const headerStrings = headers.map((h) => h.toString());
+          onStart &&
+            onStart(
+              parseInt(headerStrings[headerStrings.findIndex((h) => h == "Content-Length") + 1])
+            );
+          return true;
+        },
+        onData: (chunk) => {
+          transferredBytes += chunk.length;
+          fileStream.write(chunk);
+          onProgress && onProgress(transferredBytes);
+          return true;
+        },
+        onComplete: (trailers) => {
+          client.close();
+          onComplete && onComplete();
+          resolve();
+        },
+      }
+    );
+  });
+
+  // Writing the chain-info after the full state to ensure it is not updated
+  // in case of state download failure
   await fs.writeFile(stateInfoFile, JSON.stringify(downloadedStateInfo));
   debug(`Downloaded ${stateFileName} to ${stateFile}`);
 
@@ -78,6 +138,30 @@ export async function downloadExportedState(network: NetworkName, outPath: strin
 // Customize a Moonbeam exported state spec to make it usable locally
 // It makes Alith the main collator and restore XCMP/HRMP data.
 export async function neutralizeExportedState(inFile: string, outFile: string) {
+  await processState(inFile, outFile, [
+    new RoundManipulator((current, first, length) => {
+      return { current, first: 0, length: 100 };
+    }),
+    new AuthorFilteringManipulator(100),
+    new SudoManipulator(ALITH_ADDRESS),
+    new CollatorManipulator(ALITH_ADDRESS, ALITH_SESSION_ADDRESS),
+    new HRMPManipulator(),
+    new SpecManipulator({
+      name: `Fork Network`,
+      relayChain: `rococo-local`,
+    }),
+    new CollectiveManipulator("TechCommitteeCollective", [ALITH_ADDRESS]),
+    new CollectiveManipulator("CouncilCollective", [ALITH_ADDRESS]),
+    new ValidationManipulator(),
+    new XCMPManipulator(),
+    new BalancesManipulator([{ account: ALITH_ADDRESS, amount: 10_000n * 10n ** 18n }]),
+  ]);
+}
+
+
+// Customize a Moonbeam exported state spec to make it usable locally
+// It makes Alith the main collator and restore XCMP/HRMP data.
+export async function insertParachainCodeIntoRelay(inFile: string, outFile: string) {
   await processState(inFile, outFile, [
     new RoundManipulator((current, first, length) => {
       return { current, first: 0, length: 100 };
---
+++ src/tools/run-moonbeam-fork.ts
@@ -0,0 +1,222 @@
+// This script is expected to run against a parachain network (using launch.ts script)
+
+import moment from "moment";
+import prettyBytes from "pretty-bytes";
+import { SingleBar } from "cli-progress";
+import { runTask } from "../utils/runner";
+import yargs from "yargs";
+import chalk from "chalk";
+import fs from "fs/promises";
+import path from "path";
+import {
+  downloadExportedState,
+  NetworkName,
+  neutralizeExportedState,
+} from "../libs/helpers/state-manipulator";
+
+const argv = yargs(process.argv.slice(2))
+  .usage("Usage: $0")
+  .version("1.0.0")
+  .options({
+    network: {
+      type: "string",
+      alias: "n",
+      description: "Network to retrieve the exported state for",
+      demandOption: true,
+    },
+    latest: {
+      type: "boolean",
+      description: "Will verify if a latest snapshot is available and download it",
+      default: false,
+    },
+    "moonbeam-binary": {
+      type: "string",
+      alias: "m",
+      description: "Binary of the moonbeam node",
+      demandOption: true,
+    },
+    "polkadot-binary": {
+      type: "string",
+      alias: "p",
+      description: "Binary of the polkadot node",
+      demandOption: true,
+    },
+    "base-path": {
+      type: "string",
+      description: "Where to store the data",
+      demandOption: true,
+    },
+  }).argv;
+
+const NODE_KEYS = {
+  "1111111111111111111111111111111111111111111111111111111111111111":
+    "12D3KooWPqT2nMDSiXUSx5D7fasaxhxKigVhcqfkKqrLghCq9jxz",
+  "2222222222222222222222222222222222222222222222222222222222222222":
+    "12D3KooWLdJAwPtyQ5RFnr9wGXsQzpf3P2SeqFbYkqbfVehLu4Ns",
+  "3333333333333333333333333333333333333333333333333333333333333333":
+    "12D3KooWBRFW3HkJCLKSWb4yG6iWRBpgNjbM4FFvNsL5T5JKTqrd",
+};
+const bootNodes = Object.values(NODE_KEYS)
+  .slice(0, 3)
+  .map((peerId, key) => `/ip4/127.0.0.1/tcp/10001/p2p/${peerId}`);
+
+const main = async () => {
+  if (!argv["polkadot-binary"] || (await fs.access(argv["polkadot-binary"]).catch(() => false))) {
+    throw new Error("Missing polkadot-binary");
+  }
+  process.stdout.write(`\t - Checking polkadot binary...`);
+  const polkadotVersion = await runTask(`${argv["polkadot-binary"]} --version`);
+  process.stdout.write(` ${chalk.green(polkadotVersion.trim())} ✓\n`);
+
+  if (!argv["moonbeam-binary"] || (await fs.access(argv["moonbeam-binary"]).catch(() => false))) {
+    throw new Error("Missing moonbeam-binary");
+  }
+  process.stdout.write(`\t - Checking moonbeam binary...`);
+  const moonbeamVersion = await runTask(`${argv["moonbeam-binary"]} --version`);
+  process.stdout.write(` ${chalk.green(moonbeamVersion.trim())} ✓\n`);
+
+  process.stdout.write(`\t - Checking exported state...`);
+
+  let progressBar: SingleBar;
+  const file = await downloadExportedState(
+    argv.network as NetworkName,
+    argv["base-path"],
+    argv.latest,
+    (length) => {
+      process.stdout.write(`${chalk.yellow(`Downloading`)}\n`);
+      progressBar = new SingleBar({
+        etaAsynchronousUpdate: true,
+        fps: 5,
+        etaBuffer: 100,
+        format:
+          "CLI Progress |" +
+          chalk.yellow("{bar}") +
+          "| {percentage}% | {eta_formatted} || {value}/{total}",
+        formatTime: (value) => moment.duration(value * 1000).humanize(),
+        formatValue: (value) => (value <= 100 ? value : prettyBytes(parseInt(value))),
+      });
+      progressBar.start(length, 0);
+    },
+    (bytes) => {
+      progressBar.update(bytes);
+    },
+    () => {
+      progressBar.stop();
+      process.stdout.write(`\t - ${chalk.yellow(`Saving`)} ${argv.network} exported state...`);
+    }
+  );
+  process.stdout.write(` ${chalk.green(file)} ✓\n`);
+
+  process.stdout.write(`\t - Checking parachain id...`);
+  const paraId = parseInt(
+    await runTask(`head -100 ${file} | grep paraId  | cut -c 12- | rev | cut -c 2- | rev`)
+  );
+  process.stdout.write(` ${chalk.green(paraId)} ✓\n`);
+
+  process.stdout.write(`\t - Checking customized state...`);
+  const modFile = file.replace(/.json$/, ".mod.json");
+  if (
+    !(await fs
+      .access(modFile)
+      .then(() => true)
+      .catch(() => false))
+  ) {
+    process.stdout.write(` ${chalk.yellow(`generating`)} (3min)...`);
+    await neutralizeExportedState(file, modFile);
+    process.stdout.write(` ✓\n`);
+  }
+  process.stdout.write(` ${chalk.green(modFile)} ✓\n`);
+
+  process.stdout.write(`\t - Checking parachain wasm code...`);
+  const codeFile = path.join(argv["base-path"], `${argv.network}.code`);
+  if (
+    !(await fs
+      .access(codeFile)
+      .then(() => true)
+      .catch(() => false))
+  ) {
+    process.stdout.write(` ${chalk.yellow(`extracting`)}...`);
+    const grepLine = await runTask(
+      `grep '        "0x3a636f6465"' ${modFile} | cut -c 26- | rev | cut -c 3- | rev | tr -d '\n' | tee ${codeFile} | wc -c`
+    );
+    process.stdout.write(` ${prettyBytes(parseInt(grepLine) / 2)} ✓\n`);
+    process.stdout.write(`\t - ${chalk.yellow(`Saving`)} wasm code...`);
+  }
+  process.stdout.write(` ${chalk.green(codeFile)} ✓\n`);
+
+  process.stdout.write(`\t - Checking parachain genesis...`);
+  const genesisStateFile = path.join(argv["base-path"], `${argv.network}.genesis.state`);
+  if (
+    !(await fs
+      .access(genesisStateFile)
+      .then(() => true)
+      .catch(() => false))
+  ) {
+    process.stdout.write(` ${chalk.yellow(`exporting`)}...`);
+    await runTask(
+      `${argv["moonbeam-binary"]} export-genesis-state --chain ${modFile} | tee ${genesisStateFile}`
+    );
+  }
+  process.stdout.write(` ${chalk.green(genesisStateFile)} ✓\n`);
+
+  const parachainCode = (await fs.readFile(codeFile)).toString();
+  const genesisState = (await fs.readFile(genesisStateFile)).toString();
+
+  process.stdout.write(`\t - Checking relaychain plain spec file...`);
+  const relayPlainSpecFile = path.join(
+    argv["base-path"],
+    `rococo-${argv.network}-local-plain.json`
+  );
+  if (
+    !(await fs
+      .access(relayPlainSpecFile)
+      .then(() => true)
+      .catch(() => false))
+  ) {
+    process.stdout.write(` ${chalk.yellow(`generating`)}...`);
+    await runTask(
+      `${argv["polkadot-binary"]} build-spec --chain rococo-local --disable-default-bootnode > ${relayPlainSpecFile}`
+    );
+    process.stdout.write(` ✓\n`);
+
+    process.stdout.write(`\t   - Including parachain ${paraId} in relaychain plain specs...`);
+    let relayChainSpec = JSON.parse((await fs.readFile(relayPlainSpecFile)).toString());
+    relayChainSpec.bootNodes = bootNodes;
+    relayChainSpec.genesis.runtime.runtime_genesis_config.paras = [
+      [
+        [
+          paraId,
+          {
+            genesis_head: genesisState,
+            validation_code: parachainCode,
+            parachain: true,
+          },
+        ],
+      ],
+    ];
+    await fs.writeFile(relayPlainSpecFile, JSON.stringify(relayChainSpec, null, 2));
+    process.stdout.write(` ✓\n`);
+    process.stdout.write(`\t - ${chalk.yellow(`Saving`)} plain relaychain spec...`);
+  }
+  process.stdout.write(` ${chalk.green(relayPlainSpecFile)} ✓\n`);
+
+  process.stdout.write(`\t - Checking relaychain raw spec file...`);
+  const relayRawSpecFile = path.join(argv["base-path"], `rococo-${argv.network}-local-raw.json`);
+  if (
+    !(await fs
+      .access(relayRawSpecFile)
+      .then(() => true)
+      .catch(() => false))
+  ) {
+    process.stdout.write(` ${chalk.yellow(`generating`)}...`);
+    await runTask(
+      `${argv["polkadot-binary"]} build-spec --chain ${relayPlainSpecFile} > ${relayRawSpecFile}`
+    );
+    process.stdout.write(` ✓\n`);
+    process.stdout.write(`\t - ${chalk.yellow(`Saving`)} raw relaychain spec...`);
+  }
+  process.stdout.write(` ${chalk.green(relayRawSpecFile)} ✓\n`);
+
+};
+
+main();
