bc7ecd93c97a8ce27d49303fe00bf43c889a19e3
crystalin <alan.sapede@gmail.com>
2023-03-08 21:15:17 +0100

    Adds script to list delegations

---
+++ src/tools/list-delegations.ts
@@ -0,0 +1,123 @@
+// This script is expected to run against a parachain network (using launch.ts script)
+import yargs from "yargs";
+import { table } from "table";
+
+import {
+  getAccountIdentity,
+  getApiFor,
+  NETWORK_YARGS_OPTIONS,
+  combineRequestsPerDelegators,
+} from "..";
+
+const argv = yargs(process.argv.slice(2))
+  .usage("Usage: $0")
+  .version("1.0.0")
+  .options({
+    ...NETWORK_YARGS_OPTIONS,
+    delegator: {
+      type: "string",
+      description: "Address of the delegator",
+      required: true,
+    },
+    at: {
+      type: "number",
+      description: "Block number to look into",
+    },
+  }).argv;
+
+const main = async () => {
+  // Instantiate Api
+  const api = await getApiFor(argv);
+
+  const delegator = api.registry.createType("EthereumAccountId", argv.delegator).toString();
+
+  const blockHash = argv.at
+    ? await api.rpc.chain.getBlockHash(argv.at)
+    : await api.rpc.chain.getBlockHash();
+  const apiAt = await api.at(blockHash);
+  const specVersion = (await apiAt.query.system.lastRuntimeUpgrade())
+    .unwrap()
+    .specVersion.toNumber();
+
+  // Load asycnhronously all data
+  const dataPromise = Promise.all([
+    api.query.parachainStaking.delegatorState(delegator),
+    api.query.parachainStaking.delegationScheduledRequests.entries(),
+    specVersion >= 1500
+      ? (apiAt.query.parachainStaking.delegationScheduledRequests.entries() as any)
+      : [],
+  ]);
+
+  // Wait for data to be retrieved
+  const [delegatorStateData, delegationRequests] = await dataPromise;
+
+  if (!delegatorStateData.isSome) {
+    console.log("No delegations");
+    await api.disconnect();
+    return;
+  }
+  const delegatorState = delegatorStateData.unwrap();
+
+  const candidateNames = await Promise.all(
+    delegatorState.delegations.map((d) => getAccountIdentity(api, d.owner.toHex()))
+  );
+
+  const delegations = delegatorState.delegations;
+
+  const delegatorRequests = combineRequestsPerDelegators(specVersion, delegationRequests, [
+    delegatorState,
+  ]);
+
+  let total = 0n;
+  let revokable = 0n;
+  const data = delegations
+    .map((delegation, i) => {
+      const request = delegatorRequests[delegator.toLocaleLowerCase()]?.find(
+        (req) => req.collatorId == delegation.owner.toHex()
+      );
+      total += delegation.amount.toBigInt();
+      revokable += request ? request.amount / 10n ** 18n : 0n;
+      return [
+        delegator,
+        delegation.owner.toHex(),
+        candidateNames[i],
+        delegation.amount.toBigInt() / 10n ** 18n,
+        request ? `${request.amount / 10n ** 18n}` : "",
+      ];
+    })
+    .sort((a: any, b: any) => Number(b[3] - a[3]));
+
+  const account = await apiAt.query.system.account(delegator);
+  const tableData = ([["Delegator", "Candidate", "Name", "Delegation", "Revokable"]] as any[])
+    .concat(data)
+    .concat([
+      [
+        `free: ${account.data.free.toBigInt() / 10n ** 18n}`,
+        data.length.toString(),
+        "",
+        total / 10n ** 18n,
+        revokable > 0n ? revokable / 10n ** 18n : "",
+      ],
+    ] as any[]);
+
+  console.log(`preparing the table: ${tableData.length} entries`);
+  console.log(
+    table(tableData, {
+      drawHorizontalLine: (lineIndex: number) =>
+        lineIndex == 0 ||
+        lineIndex == 1 ||
+        lineIndex == tableData.length - 1 ||
+        lineIndex == tableData.length,
+      columns: [
+        { alignment: "left" },
+        { alignment: "left" },
+        { alignment: "left" },
+        { alignment: "right" },
+        { alignment: "right" },
+      ],
+    })
+  );
+  await api.disconnect();
+};
+
+main();
