7ecbf7a82a67453d5ca7f1a280db351067f16052
girazoki <gorka.irazoki@gmail.com>
2023-09-13 09:29:48 +0200

    Girazoki send n clear origins (#27)

---
+++ src/tools/send-clear-origin-messages-from-relay.ts
@@ -0,0 +1,86 @@
+import yargs from "yargs";
+import "@polkadot/api-augment";
+import { Keyring } from "@polkadot/api";
+
+import { getApiFor } from "../utils/networks";
+import { getAccountIdentity } from "../utils/monitoring";
+import { BN } from "@polkadot/util";
+import { isBigInt, isBn, isHex, isNumber, isU8a, u8aConcat, u8aToBn, u8aToHex, u8aToU8a } from '@polkadot/util';
+
+const argv = yargs(process.argv.slice(2))
+  .usage("Usage: $0")
+  .version("1.0.0")
+  .options({
+    url: {
+      type: "string",
+      description: "Relay Websocket url",
+      string: true,
+      demandOption: true,
+    },
+    para: {
+        type: "number",
+        description: "ParaId to which the messages are sent",
+    },
+    numMessages: {
+      type: "number",
+      description: "Number of messages to be sent",
+    },
+    privKey: {
+      type: "string",
+      description: "private key of the account to send the tx",
+    },
+  }).argv;
+
+
+const main = async () => {
+  const api = await getApiFor(argv);
+
+  let sendExtrinsic = api.tx.xcmPallet.send(
+    { V2: { parents: new BN(0), interior: { X1: { Parachain: argv.para }} } },
+    {
+      V2: [
+        { ClearOrigin: null }
+      ]
+    }
+  );
+  let Txs = [];
+
+  // If several calls, we just push alltogether to batch
+  for (let i = 0; i < argv.numMessages; i++) {
+    Txs.push(sendExtrinsic)
+  }
+
+
+  const batchCall = api.tx.utility.batchAll(Txs);
+  let account;
+  let nonce;
+  [account, nonce] = await accountWrapper(api, argv.privKey);
+  console.log(account )
+  await api.tx(batchCall.toHex()).signAndSend(account);
+
+  await api.disconnect();
+};
+
+async function start() {
+  try {
+    await main();
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+async function accountWrapper(api, privateKey) {
+  // Keyring
+  const keyring = new Keyring({ type: "sr25519" });
+
+  // Create account and get nonce
+  let account = await keyring.addFromUri(privateKey, null, "sr25519");
+  console.log(account.address)
+  const { nonce: rawNonce } = (await api.query.system.account(account.address)) as any;
+  let nonce = BigInt(rawNonce.toString());
+
+  return [account, nonce];
+}
+
+start();
