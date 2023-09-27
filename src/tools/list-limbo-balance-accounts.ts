ac4464e014bceef40fea03348f14d756315b304d
Francisco Gamundi <francisco@moonsonglabs.com>
2023-09-27 09:42:28 +0200

    First limbo

---
+++ src/tools/list-limbo-balance-accounts.ts
@@ -0,0 +1,70 @@
+// This script is expected to run against a parachain network (using launch.ts script)
+import yargs from "yargs";
+import { table } from "table";
+
+import {
+  getApiFor,
+  NETWORK_YARGS_OPTIONS,
+  getWsProviderFor,
+} from "..";
+
+const argv = yargs(process.argv.slice(2))
+  .usage("Usage: $0")
+  .version("1.0.0")
+  .options({
+    ...NETWORK_YARGS_OPTIONS
+  }).argv;
+
+const main = async () => {
+  // Instantiate Api
+  const api = await getApiFor(argv);
+
+
+  const [allLocks] = await Promise.all([
+    api.query.balances.locks.entries(),
+  ]);
+
+  const addressesToCheck: string[] = [];
+  
+  allLocks.map((lock) => {
+    if (lock[1].length > 0) {
+        addressesToCheck.push(`0x${lock[0].toHex().slice(-40)}`);
+    }
+  })
+
+  const systemAccounts = await api.query.system.account.multi(addressesToCheck);
+
+  const affectedAccounts = [];
+  systemAccounts.map(async (acc, idx) => {
+    if (acc.data.free.lt(acc.data.frozen)) {
+      affectedAccounts.push([
+        addressesToCheck[idx],
+        acc.data.free.toHuman(),
+        acc.data.reserved.toHuman(),
+        acc.data.frozen.toHuman(),
+      ]);
+    }
+  })
+
+  const tableData = (
+    [["Account", "Free", "Reserved", "Frozen"]] as any[]
+  ).concat(affectedAccounts);
+
+  console.log(`preparing the table: ${tableData.length} entries`);
+  console.log(
+    table(tableData, {
+      drawHorizontalLine: (lineIndex: number) =>
+        lineIndex == 0 || lineIndex == 1 || lineIndex == tableData.length,
+      columns: [
+        { alignment: "left" },
+        { alignment: "left" },
+        { alignment: "left" },
+        { alignment: "right" },
+        { alignment: "right" },
+      ],
+    })
+  );
+  await api.disconnect();
+};
+
+main();
